d2b6ba7990dbfe8d862c682bf230ca55f4e27f06
ArVectro <balasubramanianarjun54@gmail.com>
2024-03-26 18:38:46 -0400

    lessons11,12

---
+++ lesson11/lesson11.py
@@ -0,0 +1,9 @@
+x = 12
+y = 4
+print(y % x)
+#when you divide y by x, the remainder is _. In this case, the twoo numbers are perfect factors so the remainder is 4.
+print(x**y)
+# power (x^y)
+x = x+y
+# also x+=y - same for every operation, x*=y, x-=y, etc
+print(x)
