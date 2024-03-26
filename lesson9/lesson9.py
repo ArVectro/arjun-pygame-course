c253fc733b357e3ad6c9a0c3a7fb7917d60e05f4
ArVectro <balasubramanianarjun54@gmail.com>
2024-03-26 18:20:27 -0400

    lessons 9 & 10

--- lesson9/lesson9.py
+++ lesson9/lesson9.py
@@ -1,2 +1,9 @@
 #assign number to numA and numB and then subtract numB from numA, and print the result
 numberA = float(input("First number: "))
+numberB = float(input("Second number: "))
+sub = numberA - numberB
+print("Result: " + str(sub))
+if sub<0:
+    print("Negatice number")
+else:
+    print("Positive Number")
