87eaeaaa89188e7e176eeb43ec641e75a53c4f5a
ArVectro <balasubramanianarjun54@gmail.com>
2024-03-26 18:29:30 -0400

    lessons10,11,6

---
+++ lesson10/lesson10.py
@@ -0,0 +1,10 @@
+arjunb = "I love coding I hope I will learn fast"
+print(arjunb.lower())
+print(arjunb.upper())
+print(arjunb.find('g'))
+#"I love coding I hope I will learn fast"
+# 0123456789012
+print(arjunb.replace('fast', 'quickly'))
+print(arjunb.find('coding'))
+#"I love coding I hope I will learn fast"
+# 01234567
