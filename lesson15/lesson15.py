a4e354e6df927c5e2c053f704424c8fec5d9f8b1
ArVectro <balasubramanianarjun54@gmail.com>
2024-03-26 19:38:36 -0400

    lesson15

---
+++ lesson15/lesson15.py
@@ -0,0 +1,21 @@
+temperatures = [45, 23, 67, 98, 33]
+for item in temperatures:
+    print(item)
+    
+
+numbers = range(5,10)
+
+for number in numbers:
+    print(number) # printing numbers from 5 - 10
+
+numbers = range(10,100,5) # step = 5, instead of increasing by 1 each time, we increase by 5
+
+for number in numbers:
+    print(number) 
+    
+e = 0
+
+while  e <=5:
+    print(e)
+    print(e*'e') # prints num and that num of 'e's
+    e=e+1
