42dc56900eb2ea5f2895f9ee50b57ee0b5330be2
ArVectro <balasubramanianarjun54@gmail.com>
2024-03-27 17:06:58 -0400

    lesson18

---
+++ Section3/lesson18/lesson18.py
@@ -0,0 +1,5 @@
+def path(velocity, time):
+   print(velocity*time) # if we do print(velocity*time) we get 'None' as well because it is printing the print(velocity*time)
+
+#S = path(30,60) would also add a none to the output
+print(path(30,60))
